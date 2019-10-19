20a3996bcffb79b190d98c9dba38c21e26ba35a3
shencang <2630610009@qq.com>
2019-10-19 18:23:03 +0800

    状态栏设计

--- app/src/main/java/com/shengcangblue/blogrn/activity/WebBodyActivity.kt
+++ app/src/main/java/com/shengcangblue/blogrn/activity/WebBodyActivity.kt
@@ -12,6 +12,7 @@ import android.webkit.WebViewClient
 import android.widget.FrameLayout
 import android.widget.Toast
 import com.shengcangblue.blogrn.R
+import com.shengcangblue.blogrn.util.StatusBarUtil
 
 class WebBodyActivity : AppCompatActivity() {
     //定义变量
@@ -24,6 +25,20 @@ class WebBodyActivity : AppCompatActivity() {
         setContentView(R.layout.activity_web_body)
         supportActionBar?.hide()
         initAndSetupView()
+        //这里注意下 因为在评论区发现有网友调用setRootViewFitsSystemWindows 里面 winContent.getChildCount()=0 导致代码无法继续
+        //是因为你需要在setContentView之后才可以调用 setRootViewFitsSystemWindows
+
+        //当FitsSystemWindows设置 true 时，会在屏幕最上方预留出状态栏高度的 padding
+        StatusBarUtil.setRootViewFitsSystemWindows(this, true)
+        //设置状态栏透明
+        StatusBarUtil.setTranslucentStatus(this)
+        //一般的手机的状态栏文字和图标都是白色的, 可如果你的应用也是纯白色的, 或导致状态栏文字看不清
+        //所以如果你是这种情况,请使用以下代码, 设置状态使用深色文字图标风格, 否则你可以选择性注释掉这个if内容
+        if (!StatusBarUtil.setStatusBarDarkTheme(this, true)) {
+            //如果不支持设置深色风格 为了兼容总不能让状态栏白白的看不清, 于是设置一个状态栏颜色为半透明,
+            //这样半透明+白=灰, 状态栏的文字能看得清
+            StatusBarUtil.setStatusBarColor(this, R.color.colorPrimary)
+        }
     }
 
     // 初始化对象
@@ -103,6 +118,8 @@ class WebBodyActivity : AppCompatActivity() {
     fun showToast(message: String, length: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(this, message, length).show()
     }
+
+
 
 
 }
